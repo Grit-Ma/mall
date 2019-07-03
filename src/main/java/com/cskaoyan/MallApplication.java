bb2711bb93c30e1cb1105687cfa1fdc2a1b974cf
Hui <phoebechen@163.com>
2019-07-03 21:40:40 +0800

    sys change by cf

--- src/main/java/com/cskaoyan/MallApplication.java
+++ src/main/java/com/cskaoyan/MallApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-
+@MapperScanner(base)
 public class MallApplication {
 
     public static void main(String[] args) {
