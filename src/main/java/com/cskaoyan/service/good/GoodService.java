8ad717f11d011380eb6b47153a1383e694763e70
Hthing <294507421@qq.com>
2019-07-05 17:16:27 +0800

    添加功能

--- src/main/java/com/cskaoyan/service/good/GoodService.java
+++ src/main/java/com/cskaoyan/service/good/GoodService.java
@@ -1,12 +1,21 @@
 package com.cskaoyan.service.good;
 
+import com.cskaoyan.bean.GoodsInfo;
+import com.cskaoyan.bean.vo.CatAndBrandVo;
 import com.cskaoyan.bean.vo.PageData;
+import com.cskaoyan.bean.vo.ProductVo;
 import org.springframework.stereotype.Service;
-
 
 @Service
 public interface GoodService {
-    PageData getListData(int page, int limit, String sort, String order);
+    PageData getListData(int page, int limit, String sort, String order,String goodsSn,String name);
 
-    PageData getCommentListData(int page, int limit, String sort, String order);
+    PageData getCommentListData(int page, int limit, String sort, String order, String userId, String valueId);
+
+    ProductVo getGoodById(int id);
+
+
+    CatAndBrandVo getCatAndBrand();
+
+    int update(GoodsInfo map);
 }
