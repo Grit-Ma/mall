6c799c6378f680c42a895db8dfe7a4a486a5d376
cdy17test <cdy17test@163.com>
2019-07-08 23:37:43 +0800

    wx-login

--- src/main/java/com/cskaoyan/mall_admin/controller/config/ConfigController.java
+++ src/main/java/com/cskaoyan/mall_admin/controller/config/ConfigController.java
@@ -2,28 +2,31 @@ package com.cskaoyan.mall_admin.controller.config;
 
 import com.cskaoyan.bean.vo.ResponseVO;
 import com.cskaoyan.mall_admin.service.config.ConfigService;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
-@RequestMapping("admin")
+@RequestMapping("admin/config")
 public class ConfigController {
     @Autowired
     private ConfigService configService;
 
-    @GetMapping("config/mall")
+    @RequiresPermissions("admin:config:mall:list")
+    @GetMapping("mall")
     public ResponseVO listMall() {
         ResponseVO vo = new ResponseVO();
         Map<String, Object> data = configService.listMall();
+        vo.setErrno(0);
         vo.setErrmsg("成功");
         vo.setData(data);
         return vo;
     }
 
-
-    @PostMapping("/config/mall")
+    @RequiresPermissions("admin:config:mall:updateConfigs")
+    @PostMapping("mall")
     public ResponseVO updateMall(@RequestBody Map<String, Object> data) {
         ResponseVO vo = new ResponseVO();
         configService.updateConfig(data);
@@ -32,17 +35,19 @@ public class ConfigController {
         return vo;
     }
 
-    @GetMapping("config/express")
+    @RequiresPermissions("admin:config:express:list")
+    @GetMapping("express")
     public ResponseVO listExpress() {
         ResponseVO vo = new ResponseVO();
         Map<String, Object> data = configService.listExpress();
+        vo.setErrno(0);
         vo.setErrmsg("成功");
         vo.setData(data);
         return vo;
     }
 
-
-    @PostMapping("/config/express")
+    @RequiresPermissions("admin:config:express:updateConfigs")
+    @PostMapping("express")
     public ResponseVO updateExpress(@RequestBody Map<String, Object> data) {
         ResponseVO vo = new ResponseVO();
         configService.updateConfig(data);
@@ -51,17 +56,19 @@ public class ConfigController {
         return vo;
     }
 
-    @GetMapping("config/order")
+    @RequiresPermissions("admin:config:order:list")
+    @GetMapping("order")
     public ResponseVO listOrder() {
         ResponseVO vo = new ResponseVO();
         Map<String, Object> data = configService.listOrder();
+        vo.setErrno(0);
         vo.setErrmsg("成功");
         vo.setData(data);
         return vo;
     }
 
-
-    @PostMapping("/config/order")
+    @RequiresPermissions("admin:config:order:updateConfigs")
+    @PostMapping("order")
     public ResponseVO updateOrder(@RequestBody Map<String, Object> data) {
         ResponseVO vo = new ResponseVO();
         configService.updateConfig(data);
@@ -70,17 +77,19 @@ public class ConfigController {
         return vo;
     }
 
-    @GetMapping("config/wx")
+    @RequiresPermissions("admin:config:wx:list")
+    @GetMapping("wx")
     public ResponseVO listWx() {
         ResponseVO vo = new ResponseVO();
         Map<String, Object> data = configService.listWx();
+        vo.setErrno(0);
         vo.setErrmsg("成功");
         vo.setData(data);
         return vo;
     }
 
-
-    @PostMapping("/config/wx")
+    @RequiresPermissions("admin:config:wx:updateConfigs")
+    @PostMapping("wx")
     public ResponseVO updateWx(@RequestBody Map<String, Object> data) {
         ResponseVO vo = new ResponseVO();
         configService.updateConfig(data);
---
+++ src/main/java/com/cskaoyan/mall_wx/controller/cdy/WxUserVontroller.java
@@ -0,0 +1,4 @@
+package com.cskaoyan.mall_wx.controller.cdy;
+
+public class WxUserVontroller {
+}
