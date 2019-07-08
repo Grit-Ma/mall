14eae926625bb488297ceb2a8188e445827012c6
cdy17test <cdy17test@163.com>
2019-07-08 14:54:09 +0800

    new

--- src/main/java/com/cskaoyan/controller/config/ConfigController.java
+++ src/main/java/com/cskaoyan/controller/config/ConfigController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
+@RequestMapping("admin")
 public class ConfigController {
     @Autowired
     private ConfigService configService;
--- src/main/java/com/cskaoyan/controller/stat/StatController.java
+++ src/main/java/com/cskaoyan/controller/stat/StatController.java
@@ -5,12 +5,14 @@ import com.cskaoyan.bean.vo.StatVO;
 import com.cskaoyan.service.stat.StatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("admin")
 public class StatController {
 
     @Autowired
