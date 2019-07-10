3ded22cc8515a1f4e83e4c327508c45b02e18715
cdy17test <cdy17test@163.com>
2019-07-10 16:20:03 +0800

    groupon

--- src/main/java/com/cskaoyan/mall_wx/controller/cdy/WxGrouponController.java
+++ src/main/java/com/cskaoyan/mall_wx/controller/cdy/WxGrouponController.java
@@ -1,18 +1,29 @@
 package com.cskaoyan.mall_wx.controller.cdy;
 
 import com.cskaoyan.bean.vo.ResponseVO;
+import com.cskaoyan.bean.wx.WxGroupon;
+import com.cskaoyan.bean.wx.WxListVo;
+import com.cskaoyan.mall_wx.service.cdy.WxGrouponService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("wx/groupon")
 public class WxGrouponController {
 
-    @GetMapping("list")
-    public ResponseVO list() {
+    @Autowired
+    WxGrouponService wxGrouponService;
 
-        return null;
+    @GetMapping("list")
+    public ResponseVO list(int page,int size) {
+        ResponseVO vo = new ResponseVO();
+        WxListVo  wxListVo = wxGrouponService.list(page, size);
+        vo.setData(wxListVo);
+        return vo.ok(vo);
     }
 
     @GetMapping("my")
---
+++ src/main/java/com/cskaoyan/mall_wx/service/cdy/WxGrouponService.java
@@ -0,0 +1,7 @@
+package com.cskaoyan.mall_wx.service.cdy;
+
+import com.cskaoyan.bean.wx.WxListVo;
+
+public interface WxGrouponService {
+    WxListVo list(int page, int size);
+}
