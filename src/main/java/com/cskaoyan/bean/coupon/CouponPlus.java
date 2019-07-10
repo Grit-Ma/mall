dfb16789954febabdd2104f2bee3cb855e9c1eb8
Mtan <zengwei.mtan@gmail.com>
2019-07-10 15:50:46 +0800

    update

---
+++ src/main/java/com/cskaoyan/bean/coupon/CouponPlus.java
@@ -0,0 +1,30 @@
+package com.cskaoyan.bean.coupon;
+
+
+import java.util.List;
+
+/**
+ * created by ZengWei
+ * on 2019/7/10
+ */
+public class CouponPlus {
+    int count;
+
+    List data;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public List getData() {
+        return data;
+    }
+
+    public void setData(List data) {
+        this.data = data;
+    }
+}
